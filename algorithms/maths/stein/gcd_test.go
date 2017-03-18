8158688442c4533c8e27ce651e91513818dbdd65
Gilgamesh Nootebos <gilgamez@gmail.com>
2017-03-18 17:40:48 +0100

    chore: use exported names

--- algorithms/maths/stein/gcd_test.go
+++ algorithms/maths/stein/gcd_test.go
@@ -5,31 +5,31 @@ import (
 )
 
 func TestRecurse(t *testing.T) {
-	if recurse(14, 7) != 7 ||
-		recurse(4, 2) != 2 ||
-		recurse(31, 2) != 1 ||
-		recurse(33, 11) != 11 {
+	if Recurse(14, 7) != 7 ||
+		Recurse(4, 2) != 2 ||
+		Recurse(31, 2) != 1 ||
+		Recurse(33, 11) != 11 {
 		t.Error()
 	}
 }
 
 func TestIterative(t *testing.T) {
-	if iter(14, 7) != 7 ||
-		iter(4, 2) != 2 ||
-		iter(31, 2) != 1 ||
-		iter(33, 11) != 11 {
+	if Iter(14, 7) != 7 ||
+		Iter(4, 2) != 2 ||
+		Iter(31, 2) != 1 ||
+		Iter(33, 11) != 11 {
 		t.Error()
 	}
 }
 
 func BenchmarkRecurse(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		recurse(131313131, 12343545)
+		Recurse(131313131, 12343545)
 	}
 }
 
 func BenchmarkIter(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		iter(131313131, 12343545)
+		Iter(131313131, 12343545)
 	}
 }
